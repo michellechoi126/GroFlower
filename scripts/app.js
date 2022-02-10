b706f39d448c8fc79d2cc1311ffe5c3206082483
michellechoi126 <michellechoi126@gmail.com>
2022-02-09 21:02:05 -0800

    finish growth tracker

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,49 @@
 console.log("loaded");
 
+/* === DOM Elements === */
+const $startButton = $(".start");
+const $sunMeter = $("#sunlight");
+const $waterLevel = $("#water");
+const $airQuality = $("#air");
+const $sunButton = $("#sun-button");
+const $waterButton = $("#water-button");
+const $airButton = $("#air-button");
+
 /* === Meters === */
 
 // Growth Tracker
+timer = 20;
 
+const increaseGrowth = function() {
+    timer--;
+    if (timer === 15) {
+        $("#stage1").css("color", "limegreen");
+        $("img").attr("src", "Images/pinkFlower/pinkFlower2.png");
+    };
+    if (timer === 10) {
+        $("#stage2").css("color", "limegreen");
+        $("img").attr("src", "Images/pinkFlower/pinkFlower3.png");
+    }
+    if (timer === 5) {
+        $("#stage3").css("color", "limegreen");
+        $("img").attr("src", "Images/pinkFlower/pinkFlower4.png");
+    }
+    if (timer === 0) {
+        $("#stage4").css("color", "limegreen");
+        $("img").attr("src", "Images/pinkFlower/pinkFlower5.png");
+    }
+    if (timer < 0) {
+        return clearInterval(increaseGrowth);
+    }
+};
+
+let growthInterval = setInterval(increaseGrowth, 1000);
+
+$startButton.click(function() {
+    growthInterval();
+});
 
 // Sun Meter
-const $sunMeter = $("#sunlight");
 let sun = 100;
 
 const decreaseSun = function() {
@@ -19,7 +56,6 @@ const decreaseSun = function() {
 let sunInterval = setInterval(decreaseSun, 2000);
 
 // Water Level
-const $waterLevel = $("#water");
 let water = 100;
 
 const decreaseWater = function() {
@@ -32,7 +68,6 @@ const decreaseWater = function() {
 let waterInterval = setInterval(decreaseWater, 1000);
 
 // Air Quality
-const $airQuality = $("#air");
 let air = 100;
 
 const decreaseAir = function() {
@@ -48,30 +83,24 @@ let airInterval = setInterval(decreaseAir, 3000);
 /* === Buttons === */
 
 // Sun Button
-const $sunButton = $("#sun-button");
-
 $sunButton.click(function() {
-    if ($sunMeter.attr("value") < 101) {
+    if ($sunMeter.attr("value") < 101 && $sunMeter.attr("value") > 0) {
         sun += 10
     $sunMeter.attr("value", sun);
     };
 });
 
 // Water Button
-const $waterButton = $("#water-button");
-
 $waterButton.click(function() {
-    if ($waterLevel.attr("value") < 101) {
+    if ($waterLevel.attr("value") < 101 && $waterLevel.attr("value") > 0) {
         water += 10
     $waterLevel.attr("value", water);
     };
 });
 
 // Air Button
-const $airButton = $("#air-button");
-
 $airButton.click(function() {
-    if ($airQuality.attr("value") < 101) {
+    if ($airQuality.attr("value") < 101 && $airQuality.attr("value") > 0) {
         air += 10
     $airQuality.attr("value", air);
     };
